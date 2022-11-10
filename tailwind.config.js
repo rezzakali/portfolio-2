e8d572d04d8c6041b431321c8370a6520df6b218
rezzak ali <rezzak ali>
2022-11-10 14:01:24 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{html,js}'],
+  content: ['./*.html'],
   theme: {
     extend: {},
   },
